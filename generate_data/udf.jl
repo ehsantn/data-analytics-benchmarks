5c991c28a46bd3760861ca92014dbf3e557476f0
Ehsan Totoni <ehsan.totoni@intel.com>
2016-10-06 11:29:33 -0700

    generate data for udf

---
+++ generate_data/udf.jl
@@ -0,0 +1,24 @@
+using HDF5
+
+function generate_files(path,N)
+    hdf5_file = path*"udf.hdf5"
+    csv_file = path*"udf.csv"
+    if isfile(hdf5_file)
+        rm(hdf5_file)
+    end
+    if isfile(csv_file)
+        rm(csv_file)
+    end
+    # uniform ids in a range for load balance among nodes
+    id = rand(1:256, N)
+    x = rand(N)
+    y = rand(N)
+    h5write(hdf5_file,"/id", id)
+    h5write(hdf5_file,"/x", x)
+    h5write(hdf5_file,"/y", y)
+    writecsv(csv_file, zip(id,x,y))
+end
+
+N = 256*10^6
+path = ENV["HOME"]*"/tmp/"
+generate_files(path, N)
