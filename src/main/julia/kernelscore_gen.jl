b34d854f49b8a45d4f50f756b8062f1e13525673
Ehsan Totoni <ehsan.totoni@intel.com>
2016-12-05 14:06:20 -0800

    kernel score min variable

--- src/main/julia/kernelscore_gen.jl
+++ src/main/julia/kernelscore_gen.jl
@@ -11,7 +11,8 @@ ParallelAccelerator.ParallelIR.PIRHoistParfors(true)
     exps::Float64 = 0.0 
     @par exps(+) for i in 1:length(X)
         d = -(X[i]-points).^2./(2*b^2)
-        exps += minimum(d)-log(b*N)+log(sum(exp(d-minimum(d))))
+        m = minimum(d)
+        exps += m-log(b*N)+log(sum(exp(d-m)))
     end 
     return exps
 end
