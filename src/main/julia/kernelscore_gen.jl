6c15ed6d207249002bc32163f02e0d0d07c0d768
Ehsan Totoni <ehsan.totoni@intel.com>
2016-12-07 15:50:30 -0800

    kernel score generated min, simd

--- src/main/julia/kernelscore_gen.jl
+++ src/main/julia/kernelscore_gen.jl
@@ -1,6 +1,5 @@
 using HPAT
 using ParallelAccelerator
-ParallelAccelerator.ParallelIR.PIRLateSimplify(true)
 ParallelAccelerator.ParallelIR.PIRHoistParfors(true)
 
 @acc hpat function kernelscore_test(n)
