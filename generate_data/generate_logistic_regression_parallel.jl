641813003e19b7dde11afce3da504a25084ebd21
Ehsan Totoni <ehsan.totoni@intel.com>
2017-02-09 08:06:02 -0800

    generate txt in parallel

--- generate_data/generate_logistic_regression_parallel.jl
+++ generate_data/generate_logistic_regression_parallel.jl
@@ -1,13 +1,21 @@
 using HDF5
 using DocOpt
 using HPAT
+#HPAT.DistributedPass.set_debug_level(3)
+#HPAT.DomainPass.set_debug_level(3)
+#HPAT.CaptureAPI.set_debug_level(3)
+#using ParallelAccelerator
+#ParallelAccelerator.CGen.set_debug_level(3)
+#ParallelAccelerator.DomainIR.set_debug_level(3)
 
-@acc hpat function generate_file(hdf5_file,N)
+@acc hpat function generate_file(hdf5_file, txt_file, N)
     D = 10
     A = rand(Float64,D,N)
     Y = rand(Float64,N)
     DataSink(A, HDF5,"/points", hdf5_file)
     DataSink(Y, HDF5,"/responses", hdf5_file)
+    points = [A; ParallelAccelerator.API.reshape(Y,1,N)]
+    DataSink(points, TXT, txt_file)
 end
 
 
@@ -43,8 +51,12 @@ Options:
     if isfile(hdf5_file)
         rm(hdf5_file)
     end
+    txt_file = path*"logistic_regression.hdf5"
+    if isfile(txt_file)
+        rm(txt_file)
+    end
 
-    generate_file(hdf5_file, instances)
+    generate_file(hdf5_file, txt_file, instances)
 end
 
 main()
